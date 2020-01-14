2f75411634b1f1d83112ca3fc9d3c54fb7a50a3e
Ryan Collingwood <ryancollingwood@gmail.com>
2020-01-14 20:32:34 +1100

    Got a working windows binary!!!

--- src/model/load_model.py
+++ src/model/load_model.py
@@ -1,9 +1,28 @@
+import sys
 import os.path
 import joblib
 
 
+def resource_path(relative_path):
+    """
+    as per: https://stackoverflow.com/a/13790741/2805700
+
+    Get absolute path to resource, works for dev and for PyInstaller
+    """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+
 def load_model(model_filename):
-    if not os.path.exists(model_filename):
+
+    model_resource_path = resource_path(model_filename)
+
+    if not os.path.exists(model_resource_path):
         raise FileNotFoundError(f"Model not found at {model_filename}, have you finished model training?")
 
-    return joblib.load(model_filename)
+    return joblib.load(model_resource_path)
