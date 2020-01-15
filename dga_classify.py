1b05e832fe087d1e14f4e37f41c8cba7a713b8fd
Ryan Collingwood <ryancollingwood@gmail.com>
2020-01-15 21:41:35 +1100

    Able to pass in multiple domains from dga_classify, query_model extracts columns names from model

--- dga_classify.py
+++ dga_classify.py
@@ -7,17 +7,34 @@ if __name__ == "__main__":
     query_model = QueryModel("models/trained.model")
 
     ap = argparse.ArgumentParser()
-    ap.add_argument("domain", type=str, help="Domain to be tested")
+    #ap.add_argument("domain", type=str, help="Domain to be tested")
+    ap.add_argument("domain", nargs = '*', help="Domain(s) to be test. Either single domain or comma separated")
     ap.add_argument("-i", "--interactive", action='store_true',
                     help="Enter interactive mode to type in many domains")
 
     args = vars(ap.parse_args())
 
-    given_domain = args["domain"].strip()
-    result = query_model.predict(given_domain)
+    given_domain = args["domain"]
+    result = None
 
     if args["interactive"]:
+        if given_domain:
+            first_prediction = query_model.predict(given_domain)
+
         result = query_model.interactive()
+
+        if given_domain:
+            result = result and first_prediction
+    else:
+        if not given_domain:
+            print("Must specify a domain to be tested")
+            sys.exit(2)
+
+        result = query_model.predict(given_domain)
+
+    if result is None:
+        print("No predictions were made")
+        sys.exit(2)
 
     if not result:
         print("Non-Legit Domain(s) Detected")
--- src/model/query_model.py
+++ src/model/query_model.py
@@ -5,13 +5,29 @@ from .load_model import load_model
 class QueryModel:
     def __init__(self, model_filename):
         self.model = load_model(model_filename)
+        self.input_columns = list()
 
-    def predict(self, value):
-        given_value = str(value).split(",")
-        if not isinstance(given_value, list):
-            given_value = list(given_value)
+        # get the columns names we fed into the first step of our model
+        # so that we can feed in a Dataframe with the same columns headers
+        for transformer_step in self.model.steps[0][1].transformer_list:
+            [self.input_columns.append(x) for x in transformer_step[1].in_columns if x not in self.input_columns]
 
-        predict_df = pd.DataFrame(given_value, columns=["input"])
+    @staticmethod
+    def str_to_list(value: str):
+        if isinstance(value, list):
+            return value
+
+        result = str(value).split(",")
+        if not isinstance(result, list):
+            result = list(result)
+
+        return result
+
+    def predict(self, values):
+        for key in values:
+            values[key] = self.str_to_list(values[key])
+
+        predict_df = pd.DataFrame(values, columns=self.input_columns)
 
         results = self.model.predict(predict_df)
         predict_df["result"] = results
@@ -20,9 +36,23 @@ class QueryModel:
         return all(predict_df["result"] == "legit")
 
     def get_input(self):
-        given_value = input(">").strip()
+        given_inputs = dict()
+        for column in self.input_columns:
+            given_inputs[column] = input(f"{column}>").strip()
 
-        return given_value
+        return self.validate_input(given_inputs)
+
+    def validate_input(self, given_inputs):
+        n_elements = None
+        for key in given_inputs:
+            given_inputs[key] = self.str_to_list(given_inputs[key])
+            if n_elements is not None:
+                if n_elements != len(given_inputs[key]):
+                    print("Must enter an equal number of elements.")
+                    return self.get_input()
+            else:
+                n_elements = len(given_inputs[key])
+        return given_inputs
 
     def interactive(self):
         last_result = None
