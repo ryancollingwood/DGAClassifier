d12171726f4e54552c1b57277db0cad9ce4913e4
Ryan Collingwood <ryancollingwood@gmail.com>
2020-01-17 08:16:47 +1100

    update setup script to be more verbose during training

--- setup/linux.sh
+++ setup/linux.sh
@@ -1,4 +1,4 @@
 #!
 sudo pip3 install virtualenv
 virtualenv .venv
-source .venv/bin/activate; pip3 install -r requirements.txt; pytest unittests; pytest integrationtests; python train_model.py -p data/raw/dga_domains.csv -o models; python test_model.py; python dga_classify.py -i;
+source .venv/bin/activate; pip3 install -r requirements.txt; pytest unittests; pytest integrationtests; python train_model.py -p data/raw/dga_domains.csv -o models -v 2pyth; python test_model.py; python dga_classify.py -i;
--- setup/windows.bat
+++ setup/windows.bat
@@ -5,6 +5,6 @@ pip install -r requirements.txt
 ipython kernel install --user --name=domain_generation_algorithm_classifier
 pytest unittests
 pytest integrationtests
-python train_model.py -p data/raw/dga_domains.csv -o models
+python train_model.py -p data/raw/dga_domains.csv -o models -v 2
 python test_model.py
 python dga_classify.py -i
